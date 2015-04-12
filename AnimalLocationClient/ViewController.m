622b49ab59104fc40a8d693ea9122dd78a1da4f5
董剑雄 <dongjianxiong@github.umeng.com>
2015-04-12 10:21:28 +0800

    Initial Commit

---
+++ AnimalLocationClient/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  AnimalLocationClient
+//
+//  Created by umeng on 15-4-12.
+//  Copyright (c) 2015年 umeng. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
